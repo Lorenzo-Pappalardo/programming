a032de96354cc0bc42018da8855adf09e3582fa8
Lorenzo <lorenzopappalardo7@gmail.com>
2019-11-16 10:34:09 +0100

    Couldn't resolve Sim_ITACPC

--- Sim_ITACPC.cpp
+++ Sim_ITACPC.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 class Node {
@@ -12,8 +13,13 @@ public:
         this->meters = meters;
         this->movement = movement;
     }
+
     unsigned short getIndex() { return index; }
+
     unsigned short getMeters() { return meters; }
+
+    unsigned short getMovement() { return movement; }
+
     void update() { meters += movement; }
 };
 
@@ -24,27 +30,27 @@ void swap(Node **a, int i, int j) {
 }
 
 int partition(Node **a, int start, int end) {
-	Node *x = a[start];
-	int i = start - 1;
+    Node *x = a[start];
+    int i = start - 1;
     int j = end + 1;
     do {
-    	do j--;
-		while (x->getMeters() < a[j]->getMeters());
+        do j--;
+        while (x->getMeters() < a[j]->getMeters());
 
         do i++;
         while (x->getMeters() > a[i]->getMeters());
 
         if (i < j) swap(a, i, j);
-	} while (i < j);
+    } while (i < j);
     return j;
 }
 
 void quickSort(Node **a, int start, int end) {
-	int mid;
-	if (start < end) {
-		mid = partition(a, start, end);
-		quickSort(a, start, mid);
-		quickSort(a, mid+1, end);
+    int mid;
+    if (start < end) {
+        mid = partition(a, start, end);
+        quickSort(a, start, mid);
+        quickSort(a, mid + 1, end);
     }
 }
 
@@ -59,10 +65,10 @@ int main() {
     unsigned short N;
     cout << "Insert N: ";
     cin >> N;
-    
-    Node **arr = new Node*[N];
 
-    for (int i=0; i<N; i++) {
+    Node **arr = new Node *[N];
+
+    for (int i = 0; i < N; i++) {
         unsigned short a, b;
         cin >> a;
         cin >> b;
@@ -72,20 +78,10 @@ int main() {
     //quickSort(arr, 0, N - 1);
     //print(cout, arr, N);
 
-    unsigned int start = 0;
-
-    while (start != N - 2) {
+    while (N > 1) {
         for (int i = 0; i < N; i++) arr[i]->update();
-        quickSort(arr, start + 1, N - 1);
-        unsigned short mid = N - start;
-        if (mid % 2 == 1) mid = floor((N - start) / 2);
-        else mid = floor((N - start) / 2) - 1;
-        if (arr[mid]->getMeters() == arr[mid + 1]->getMeters() && arr[mid]->getIndex() > arr[mid + 1]->getIndex()) {
-            swap(arr, mid, mid + 1);
-        }
-        start += mid;
+        quickSort(arr, 0, N - 1);
+        //RIP
     }
-
-    //print(cout, arr, N);
-    cout << arr[N - 1]->getIndex() << endl;
+    print(cout, arr, N);
 }
