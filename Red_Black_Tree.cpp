8d4eb07301381b0a3545f897ed32599b05a903e2
Lorenzo <lorenzopappalardo7@gmail.com>
2019-12-24 12:19:08 +0100

    SIGSEGV on both files

--- Red_Black_Tree.cpp
+++ Red_Black_Tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 static const int BLACK=0;
@@ -10,7 +11,7 @@ class Node {
     int color;
     Node<T> *parent, *left, *right;
 public:
-    Node(T key, int color) {
+    Node(T key, int color = RED) {
         this->key = key;
         this->color = color;
         parent = left = right = nullptr;
@@ -22,7 +23,7 @@ public:
 
     void setPointer(Node<T> *pointer, char choice) {
         if (choice == 'p') parent = pointer;
-        else if (choice = 'l') left = pointer;
+        else if (choice == 'l') left = pointer;
         else if (choice == 'r') right = pointer;
         else cerr << "Invalid choice!";
     }
@@ -48,6 +49,97 @@ class RBT {
     Node<T> *root;
     string visit;
 
+    bool isLeftChild(Node<T> *node) { return node == node->getPointer('p')->getPointer('l'); }
+
+    Node<T> *getUncle(Node<T> *node) {
+        if (!node->getPointer('p') || !node->getPointer('p')->getPointer('p')) return nullptr;
+        if (isLeftChild(node->getPointer('p'))) { return node->getPointer('p')->getPointer('p')->getPointer('r'); }
+        return node->getPointer('p')->getPointer('p')->getPointer('l');
+    }
+
+    void swapColors(Node<T> *n1, Node<T> *n2) {
+        int tmp = n1->getColor();
+        n1->setColor(n2->getColor());
+        n2->setColor(tmp);
+    }
+
+    void insertNodeUp(Node<T> *before, Node<T> *node) {
+        if (before->getPointer('p')) {
+            if (isLeftChild(before)) {
+                before->getPointer('p')->setPointer(node, 'l');
+            } else {
+                before->getPointer('p')->setPointer(node, 'r');
+            }
+        } else root = node;
+        node->setPointer(before->getPointer('p'), 'p');
+        before->setPointer(node, 'p');
+    }
+
+    void rightRotation(Node<T> *node) {
+        Node<T> *newParent = node->getPointer('l');
+        insertNodeUp(node, newParent);
+        node->setPointer(newParent->getPointer('r'), 'l');
+        if (newParent->getPointer('r')) newParent->getPointer('r')->setPointer(node, 'p');
+        newParent->setPointer(node, 'r');
+    }
+
+    void leftRotation(Node<T> *node) {
+        Node<T> *newParent = node->getPointer('r');
+        insertNodeUp(node, newParent);
+        node->setPointer(newParent->getPointer('l'), 'r');
+        if (newParent->getPointer('l')) newParent->getPointer('l')->setPointer(node, 'p');
+        newParent->setPointer(node, 'l');
+    }
+
+    void insertFixUp(Node<T> *node) {
+        do {
+            if (node == root) {
+                node->setColor(BLACK);
+            } else {
+                Node<T> *p = node->getPointer('p');
+                Node<T> *gp = p->getPointer('p');
+                Node<T> *u = getUncle(node);
+                if (p->getColor() != BLACK) {
+                    if (u != nullptr && u->getColor() == RED) {
+                        p->setColor(BLACK);
+                        gp->setColor(BLACK);
+                        u->setColor(BLACK);
+                        node = gp;
+                    } else {
+                        if (isLeftChild(p)) {
+                            if (isLeftChild(node))
+                                swapColors(p, gp);
+                            else {
+                                leftRotation(p);
+                                swapColors(node, gp);
+                            }
+                            rightRotation(gp);
+                        } else {
+                            if (isLeftChild(node)) {
+                                rightRotation(p);
+                                swapColors(node, gp);
+                            } else swapColors(p, gp);
+                            leftRotation(gp);
+                        }
+                        return;
+                    }
+                }
+            }
+        } while(true);
+    }
+
+    Node<T> *search(T key) {
+        Node<T> *tmp = root;
+        Node<T> *parent = nullptr;
+        while(tmp) {
+            if (key == tmp->getKey()) return tmp;
+            parent = tmp;
+            if (key < tmp->getKey()) tmp = tmp->getPointer('l');
+            else tmp = tmp->getPointer('r');
+        }
+        return parent;
+    }
+
     void _print(ostream &os, Node<T> *tmp) {
         if (tmp && visit == "in") {
             _print(os, tmp->getPointer('l'));
@@ -62,6 +154,7 @@ class RBT {
             _print(os, tmp->getPointer('r'));
             os << '(' << tmp->getKey() << ',' << tmp->getColor() << ") ";
         }
+        os << endl;
     }
 public:
     RBT() { root = nullptr; }
@@ -70,7 +163,46 @@ public:
         this->visit = visit;
         _print(os, root);
     }
+
+    RBT *insert(T key) {
+        Node<T> *newNode = new Node<T>(key);
+        if (!root){
+            newNode->setColor(BLACK);
+            root = newNode;
+        } else {
+            Node<T> *tmp = search(key);
+            if (tmp->getKey() != key) {
+                newNode->setPointer(tmp, 'p');
+                if (key < tmp->getKey())
+                    tmp->setPointer(newNode, 'l');
+                else tmp->setPointer(newNode, 'r');
+                insertFixUp(newNode);
+            }
+        }
+        return this;
+    }
 };
 
 int main() {
+    fstream input, output;
+    input.open("input.txt", fstream::in);
+    output.open("output.txt", fstream::out);
+
+    string type;
+    int N;
+    string visit;
+
+    while (input >> type >> N >> visit) {
+        if (type == "int") {
+            RBT<int> *t = new RBT<int>();
+            for (int i=0; i<N; i++) {
+                int value;
+                input >> value;
+                t->insert(value);
+            }
+            if (visit == "inorder") t->print(output, "in");
+            else if (visit == "preorder") t->print(output, "pre");
+            else if (visit == "postorder") t->print(output, "post");
+        }
+    }
 }
--- Rotations_BST.cpp
+++ Rotations_BST.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -34,11 +35,43 @@ template<class T>
 class BST {
     Node<T> *root;
 
+    void _insert(T key) {
+        Node<T> *tmp = root;
+        Node<T> *p = nullptr;
+        auto *newnode = new Node<T>(key);
+        while (tmp) {
+            p = tmp;
+            if (key <= tmp->getKey()) tmp = tmp->getLeft();
+            else tmp = tmp->getRight();
+        }
+        if (p) {
+            if (key <= p->getKey()) p->setLeft(newnode);
+            else p->setRight(newnode);
+            newnode->setParent(p);
+        } else root = newnode;
+    }
+
     void _inorder(ostream &os, Node<T> *tmp) {
         if (tmp) {
             _inorder(os, tmp->getLeft());
             os << tmp->getKey() << ' ';
             _inorder(os, tmp->getRight());
+        }
+    }
+
+    void _preorder(ostream &os, Node<T> *tmp) {
+        if (tmp) {
+            os << tmp->getKey() << ' ';
+            _preorder(os, tmp->getLeft());
+            _preorder(os, tmp->getRight());
+        }
+    }
+
+    void _postorder(ostream &os, Node<T> *tmp) {
+        if (tmp) {
+            _postorder(os, tmp->getLeft());
+            _postorder(os, tmp->getRight());
+            os << tmp->getKey() << ' ';
         }
     }
 
@@ -88,42 +121,150 @@ class BST {
         return this;
     }
 
+    bool isLeftChild(Node<T> *node) { return node == node->getParent()->getLeft(); }
+
+    void insertNodeUp(Node<T> *before, Node<T> * node) {
+        if (before->getParent()) {
+            if (isLeftChild(node)) before->getParent()->setLeft(node);
+            else before->getParent()->setRight(node);
+        } else root = node;
+        node->setParent(before->getParent());
+        before->setParent(node);
+    }
+
 public:
     BST() { root = nullptr; }
 
     BST *insert(T key) {
-        Node<T> *tmp = root;
-        Node<T> *p = nullptr;
-        auto *newnode = new Node<T>(key);
-        while (tmp) {
-            p = tmp;
-            if (key <= tmp->getKey()) tmp = tmp->getLeft();
-            else tmp = tmp->getRight();
-        }
-        if (p) {
-            if (key <= p->getKey()) p->setLeft(newnode);
-            else p->setRight(newnode);
-            newnode->setParent(p);
-        } else root = newnode;
+        _insert(key);
         return this;
     }
 
     BST *del(T key) { return _del(key, root); }
 
+    Node<T> *search(T key) { return _search(key, root); }
+
+    void leftRotate(Node<T> *node) {
+        Node<T> *newParent = node->getRight();
+        insertNodeUp(node, newParent);
+        node->setRight(newParent->getLeft());
+        if (newParent->getLeft()) newParent->getLeft()->setParent(node);
+        newParent->setLeft(node);
+    }
+
+    void rightRotate(Node<T>* node)
+	{
+		Node<T> *newParent = node->getLeft();
+        insertNodeUp(node, newParent);
+        node->setLeft(newParent->getRight());
+        if (newParent->getRight()) newParent->getRight()->setParent(node);
+        newParent->setRight(node);
+	}
+
     void inorder(ostream &os) {
         _inorder(os, root);
+        os << endl;
+    }
+
+    void preorder(ostream &os) {
+        _preorder(os, root);
+        os << endl;
+    }
+
+    void postorder(ostream &os) {
+        _postorder(os, root);
         os << endl;
     }
 };
 
 int main() {
-    auto *t = new BST<int>();
-    t->insert(5)->insert(4)->insert(9)->insert(7)->insert(37)->insert(23);
-    t->inorder(cout);
-    t->del(4);
-    t->del(9);
-    t->del(37);
-    t->del(5);
-    cout << "RES:\n";
-    t->inorder(cout);
+    fstream input, output;
+    input.open("input.txt", fstream::in);
+    output.open("output.txt", fstream::out);
+
+    string type;
+    int N;
+    int R;
+    string visit;
+
+    while(input >> type >> N >> R >> visit) {
+        if (type == "int") {
+            BST<int> *t = new BST<int>();
+            for (int i=0; i<N+R; i++) {
+                string value;
+                input >> value;
+                if (value[0] == 'i') {
+                    value = value.substr(4);
+                    stringstream ss;
+                    ss << value;
+                    int tmp;
+                    ss >> tmp;
+                    t->insert(tmp);
+                } else if (value[0] == 'c') {
+                    value = value.substr(5);
+                    stringstream ss;
+                    ss << value;
+                    int tmp;
+                    ss >> tmp;
+                    t->del(tmp);
+                } else if (value[0] == 'l') {
+                    value = value.substr(5);
+                    stringstream ss;
+                    ss << value;
+                    int tmp;
+                    ss >> tmp;
+                    t->leftRotate(t->search(tmp));
+                } else {
+                    value = value.substr(6);
+                    stringstream ss;
+                    ss << value;
+                    int tmp;
+                    ss >> tmp;
+                    t->rightRotate(t->search(tmp));
+                }
+            }
+            if (visit == "inorder") { t->inorder(output); }
+            else if (visit == "preorder") { t->preorder(output); }
+            else { t->postorder(output); }
+        }
+        if (type == "double") {
+            BST<double> *t = new BST<double>();
+            for (int i=0; i<N+R; i++) {
+                string value;
+                input >> value;
+                if (value[0] == 'i') {
+                    value = value.substr(4);
+                    stringstream ss;
+                    ss << value;
+                    double tmp;
+                    ss >> tmp;
+                    t->insert(tmp);
+                } else if (value[0] == 'c') {
+                    value = value.substr(5);
+                    stringstream ss;
+                    ss << value;
+                    double tmp;
+                    ss >> tmp;
+                    t->del(tmp);
+                } else if (value[0] == 'l') {
+                    value = value.substr(5);
+                    stringstream ss;
+                    ss << value;
+                    double tmp;
+                    ss >> tmp;
+                    t->leftRotate(t->search(tmp));
+                } else {
+                    value = value.substr(6);
+                    stringstream ss;
+                    ss << value;
+                    double tmp;
+                    ss >> tmp;
+                    t->rightRotate(t->search(tmp));
+                }
+            }
+            if (visit == "inorder") { t->inorder(output); }
+            else if (visit == "preorder") { t->preorder(output); }
+            else { t->postorder(output); }
+        }
+    }
 }
