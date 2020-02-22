2e28290a0bfe1e166488c16e6c1d58f876b02f1e
Lorenzo <lorenzopappalardo7@gmail.com>
2020-02-22 12:39:47 +0100

    Reworked Counting_Sort, however it only works for int, bool and double types

--- Counting_Sort.cpp
+++ Counting_Sort.cpp
@@ -1,47 +1,38 @@
-#include <iostream>
 #include <fstream>
-#include <cmath>
+#include <iostream>
 
 using namespace std;
-
-template<class T>
-T *C;
 
 template<class T>
 void print(ostream &os, T *a, int N) {
     for (int i = 0; i < N; i++)
         os << a[i] << ' ';
-    //os << endl;
 }
 
 template<class T>
-void countingSort(T *a, int N) {
-    T min = a[0];
+void countingSort(ostream &os, T *a, int N) {
     T max = a[0];
-    for (int i = 0; i < N; i++) {
+    T min = a[0];
+    for (int i = 1; i < N; i++) {               //Finding max and min
         if (a[i] > max) max = a[i];
         if (a[i] < min) min = a[i];
     }
-    T dim = max - min + 1;
-    T sorted[dim];
-    for (int i = 0; i < dim; i++)
-        sorted[i] = 0;
-    for (int i = 0; i < N; i++)
-        sorted[a[i] - min]++;
-    C<T> = new T[dim];
-    C<T>[0] = 0;
-    for (int i = 1; i < dim; i++) {
-        C<T>[i] = C<T>[i - 1] + sorted[i - 1];
+    T size = max - min + 1;
+    T count[size];
+    for (int i = 0; i < size; i++)              //Initializing count array
+        count[i] = 0;
+    for (int i = 0; i < N; i++)                 //Counting elements
+        count[a[i] - min]++;
+    for (int i = 1; i < size; i++)              //Summing frequencies
+        count[i] += count[i - 1];
+    T tmp[N];
+    for (int i = N - 1; i >= 0; i--) {          //Sorting temporary array
+        tmp[count[a[i] - min] - 1] = a[i];
+        count[a[i] - min]--;
     }
-    int i = 0;
-    int j = 0;
-    while (i < N) {
-        while (sorted[j] > 0) {
-            a[i++] = j + min;
-            sorted[j]--;
-        }
-        j++;
-    }
+    for (int i = 0; i < N; i++)                 //Copying sorted elements into the original array
+        a[i] = tmp[i];
+    print(os, count, size);
 }
 
 int main() {
@@ -49,18 +40,36 @@ int main() {
     input.open("input.txt", fstream::in);
     output.open("output.txt", fstream::out);
 
-    string type; //= "int";
+    string type;
     int N;
 
-    while (input >> /*type >>*/ N) {
-        if (type == "int") {
-            int *arr = new int[N];
+    while (input >> type >> N) {
+        if (type == "int" || type == "bool") {
+            int a[N];
             for (int i = 0; i < N; i++)
-                input >> arr[i];
-            countingSort(arr, N);
-            print(output, C<int>, Cs);
-            print(output, arr, N);
+                input >> a[i];
+            countingSort(output, a, N);
+            print(output, a, N);
+            output << endl;
+        } else if (type == "double") {
+            double a[N];
+            for (int i = 0; i < N; i++)
+                input >> a[i];
+            int tmp[N];
+            for (int i = 0; i < N; i++)
+                tmp[i] = (int) (a[i] * 10);
+            countingSort(output, tmp, N);
+            for (int i = 0; i < N; i++)
+                a[i] = (double) (tmp[i]) / 10;
+            print(output, a, N);
+            output << endl;
+        } else {
+            char a[N];
+            for (int i = 0; i < N; i++)
+                input >> a[i];
             output << endl;
         }
     }
+    input.close();
+    output.close();
 }
