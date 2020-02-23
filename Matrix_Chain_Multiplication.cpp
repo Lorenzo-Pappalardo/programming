a256e2a942c5cd3a3c520c27685ad4af41d3556e
Lorenzo <lorenzopappalardo7@gmail.com>
2020-02-23 10:38:56 +0100

    Refined code by changing some variables'name, to better understand the meaning

--- Matrix_Chain_Multiplication.cpp
+++ Matrix_Chain_Multiplication.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int MCM(vector<int> &chain) {
     int chain_length = chain.size() - 1;
     vector<vector<int>> LUT = vector<vector<int>>(chain_length, vector<int>(chain_length, 0));
-    for (int j = 1; j < chain_length; j++) {            //Columns
-        for (int i = 0; i < chain_length - j; i++) {    //Rows
-            int k = i + j;
-            for (int l = i; l < k; l++) {
-                int m = LUT[i][l] + LUT[l + 1][k] + chain[i] * chain[l + 1] * chain[k + 1];
-                if (LUT[i][k] == 0 || m < LUT[i][k]) {
-                    LUT[i][k] = m;
-                    LUT[k][i] = l;
+    for (int delta = 1; delta < chain_length; delta++) {
+        for (int i = 0; i < chain_length - delta; i++) {    //Rows
+            int j = delta + i;                              //Columns
+            for (int k = i; k < j; k++) {
+                int m = LUT[i][k] + LUT[k + 1][j] + chain[i] * chain[k + 1] * chain[j + 1];
+                if (LUT[i][j] == 0 || m < LUT[i][j]) {
+                    LUT[i][j] = m;
+                    LUT[j][i] = k;
                 }
             }
         }
@@ -34,16 +34,11 @@ int main() {
         vector<int> chain;
         for (int i = 0; i < N; i++) {
             input >> trash >> r >> trash >> c >> trash;
-            //cout << r << 'x' << c << " | ";   //Checking...
             if (i == 0) {
                 chain.push_back(r);
                 chain.push_back(c);
             } else chain.push_back(c);
         }
-        /*for (int i = 0; i < chain.size(); i++) {
-            cout << chain[i] << ' ';
-        }
-        cout << endl; Checking...*/
         output << MCM(chain) << endl;
     }
 }
